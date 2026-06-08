d737f3d4c182152cc69a54a5b42f175219322b84
Bhairava-Raju <n.bhairavaraju99@gmail.com>
2026-06-08 16:00:16 +0530

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool ans = false;
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(root == NULL && subRoot == NULL) return true;
+        if(root == NULL || subRoot == NULL) return false;
+        return pre(root, subRoot);
+    }
+    bool pre(TreeNode* root, TreeNode* subRoot){
+        if(root == NULL) return false;
+        if(root->val == subRoot->val) ans = check(root, subRoot);
+        if(ans == true) return true;
+        pre(root->left, subRoot);
+        if(ans == true) return true;
+        pre(root->right, subRoot);
+        if(ans == true) return true;
+        return false;
+    }
+    bool check(TreeNode* p, TreeNode* q){
+        if(p == NULL && q == NULL) return true;
+        else if(p == NULL || q == NULL) return false;
+        if(p->val != q->val) return false;
+        bool left = check(p->left, q->left);
+        if(left == false) return false;
+        bool right = check(p->right, q->right);
+        if(right == false) return false;
+        return true;
+    }
+};
